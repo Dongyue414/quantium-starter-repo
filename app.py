4d7052dd63a3284c4e15db3699ba33c19d2457b9
Dongyue414 <zhangdongyue84@gmail.com>
2023-04-16 17:05:49 -0400

    04/16

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -7,17 +7,36 @@ app = Dash(__name__)
 df = pd.read_table('new_data.csv',sep=',')
 df = df.sort_values(by='date')
 
-fig = px.line(df, x='date', y="sales")
+fig = px.line(df, x='date', y="sales",  title="Pink Morsel Sales")
 
 app.layout = html.Div(children=[
     html.H1(children='data visiualization for Sales vs. Date'),
 
+    dcc.RadioItems(
+                ['north', 'east', 'south', 'west', 'all'],
+                'all',
+                id='region',
+                inline=True
+            ),
 
     dcc.Graph(
-        id='example-graph',
+        id='graph',
         figure=fig
     )
 ])
+
+@app.callback(
+    Output('graph', 'figure'),
+    Input('region', 'value'))
+
+def update_graph(region):
+    if region != 'all':
+        dff = df[df['region'] == region]
+    else:
+        dff = df
+    fig = px.line(dff, x='date', y="sales",  title="Pink Morsel Sales")
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
